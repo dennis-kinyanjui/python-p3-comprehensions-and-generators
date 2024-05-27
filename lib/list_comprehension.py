8df780651ade70ee4d504a4ff578d98aad2c16ae
Dennis <dennis.kinyanjui@student.moringaschool.com>
2024-05-28 01:02:57 +0300

    comlete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
-#!/usr/bin/env python3
+def return_evens(numbers):
+    """Returns a list of even elements from the input list using list comprehension."""
+    return [num for num in numbers if num % 2 == 0]
 
-def return_evens(num_list):
-    pass
+def make_exclamation(sentences):
+    """Returns a list of sentences with exclamation marks at the end using list comprehension."""
+    return [sentence + "!" for sentence in sentences]
 
-def make_exclamation(sentence_list):
-    pass
+# Example usage:
+#if __name__ == "__main__":
+#    print(return_evens([0, 1, 3, 5, 7, 8, 9]))  # Output: [0, 8]
+#   print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))  # Output: ["Hello!", "I'm doing great!", "Python is fun!"]
